d2023d55d2b09b5dc6a6e7e81bff60e4581781e5
Jesu <garcialopezjesus36@gmail.com>
2017-03-16 13:53:15 +0100

    principio de metodo suma

--- src/main/java/com/spring/calculator/controller/CalculatorController.java
+++ src/main/java/com/spring/calculator/controller/CalculatorController.java
@@ -2,14 +2,17 @@ package com.spring.calculator.controller;
 
 
 import org.springframework.stereotype.Controller;
-
-
-
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CalculatorController {
 	
-
+	@PostMapping("/s")
+	public int Sumar (@ModelAttribute("input1") Integer input1, Model model,@ModelAttribute("input2") Integer input2)
+	{
 }
 	
 	
