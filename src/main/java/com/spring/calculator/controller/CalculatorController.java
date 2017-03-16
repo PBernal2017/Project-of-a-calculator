dae307cbdf6df98d823893cce8bb07b9010e28ab
Marcos <lester.yeste@gmail.com>
2017-03-16 13:57:21 +0100

    controllerMultiplicacion

--- src/main/java/com/spring/calculator/controller/CalculatorController.java
+++ src/main/java/com/spring/calculator/controller/CalculatorController.java
@@ -2,12 +2,28 @@ package com.spring.calculator.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 
 @Controller
+@RequestMapping(value="/calc")
 public class CalculatorController {
 
+ 
 	
-	
+	@PostMapping(value="/M")
+	public String multiplicacion(Model model, @ModelAttribute("input1")int input1,@ModelAttribute("input2")int input2) {
+		Integer resultado= (input1 * input2);
+		model.addAttribute("equal", resultado);
+		
+		
+		return "calc";
+		
+		
+	}
+}
