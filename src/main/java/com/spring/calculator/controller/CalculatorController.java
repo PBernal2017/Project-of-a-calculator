dd0e5d86de843d0afcabd78bfd41f2ff3a2de2fe
Jesu <garcialopezjesus36@gmail.com>
2017-03-16 14:09:58 +0100

    temp

--- src/main/java/com/spring/calculator/controller/CalculatorController.java
+++ src/main/java/com/spring/calculator/controller/CalculatorController.java
@@ -3,31 +3,29 @@ package com.spring.calculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-=======
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
->>>>>>> 3fe1014f1793592118d23a7b28fb3da8320512b8
-
 @Controller
 @RequestMapping(value="/calc")
 public class CalculatorController {
-<<<<<<< HEAD
 	
-	@PostMapping("/s")
-	public int Sumar (@ModelAttribute("input1") Integer input1, Model model,@ModelAttribute("input2") Integer input2)
+	@PostMapping("/S")
+	public String suma (@ModelAttribute("input1") int input1, Model model,@ModelAttribute("input2") int input2)
 	{
+		Integer resultado= (input1 + input2);
+		model.addAttribute("equal", resultado);
+		
+		
+		return "calc";
 }
-
-	
-=======
 
 
  
@@ -47,4 +45,4 @@ public class CalculatorController {
 }
 	
 
->>>>>>> 3fe1014f1793592118d23a7b28fb3da8320512b8
+
